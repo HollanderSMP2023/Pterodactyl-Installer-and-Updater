f2d03b09921bc49a0b93025e2cf08448cc0266c3
HollanderSMPW <111183562+HollanderSMPW@users.noreply.github.com>
2022-12-30 16:54:59 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,61 +1,52 @@
 #!/bin/bash
 
 set -e
-SCRIPT_VERSION="V1.10.1.0.0"
+SCRIPT_VERSION="V1"
 
 clear
 echo
 #print_brake 70
-echo "* Pterodactyl-Freestuff-Installer Script @ $SCRIPT_VERSION"
+echo "* Pterodactyl-Updater @ $SCRIPT_VERSION"
 echo
 echo "* Copyright (C) 2021 - 2022."
-echo "* https://github.com/Sigma-Production/PteroFreeStuffinstaller"
+echo "* https://github.com/HollanderSMPW/Pterodactyl-Updater/"
 echo
 echo "* This script is not associated with the official Pterodactyl Project."
 #print_brake 70
 echo
-echo "1. Install DarkNColor"
-echo "2. Install Addons"
-echo "3. Install Animated theme"
-echo "4. Install MinecraftTheme"
-echo "5. Install Animated Login theme"
-echo "6. Install Sidebar (Unstable)"
-echo "7. Restore from backup"
-echo "8. Close / Cancel"
+echo "1. Update some stuff"
+echo "2. Wings debug"
+echo "3. Install DarkNred Theme"
+echo "4. Backup the panel files"
+echo "5. Restore from backup"
+echo "6. Close / Cancel"
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
+    echo "Update some stuff..."
     bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/DarkNColor.sh)
 fi
 if [ $choice == "2" ]
     then
+    echo "Wings debug..."
     bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/Addons.sh)
 fi
 if [ $choice == "3" ]
     then
-    echo "Installing Particles themes..."
+    echo "Install DarkNred Theme..."
     bash <(curl -sSL https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/ParticleJS/install.sh)
 fi
 if [ $choice == "4" ]
     then
-    echo "Installing Minecraft Theme..."
+    echo "Backup the panel files..."
     bash <(curl -sSL https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/mcTheme/install.sh)
 fi
 if [ $choice == "5" ]
     then
-    echo "Installing Animated login"
+    echo "Restore from backup..."
     bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/login/install.sh)
 fi
 if [ $choice == "6" ]
-    then
-    echo "Installing Sidebar..."
-    bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/Sidebar/install.sh)
-fi
-if [ $choice == "7" ]
-    then
-    bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/restore.sh)
-fi
-if [ $choice == "8" ]
     then
     echo "Canceling..."
     exit 0
